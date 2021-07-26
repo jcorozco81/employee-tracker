a380d2a95eaabc8a5fddacee0bf7d7d14c2142f4
jcorozco81 <jcorozco@gmail.com>
2021-07-26 00:24:39 -0400

    Removed comments, added init() function as required. Fixed Modify record

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const actionOptions2 = ["Roles", "Departments", "Employee"];
 const actionOptions1 = ["View", "Add", "Modify", "Exit"]; //Add delete
 
 let departmentList;
-const departmentListQuerry = () => {db.query('SELECT department_name FROM department', (err, res) => {
+const departmentListQuery = () => {db.query('SELECT department_name FROM department', (err, res) => {
   if (err) throw err;
   departmentList = res.map(function (obj) {
     return obj.department_name;
@@ -15,11 +15,24 @@ const departmentListQuerry = () => {db.query('SELECT department_name FROM depart
 })};
 
 let roleList;
-const roleListQuerry = () => {db.query('SELECT title FROM roles', (err, res) => {
+const roleListQuery = () => {db.query('SELECT title FROM roles', (err, res) => {
   if (err) throw err;
-  roleList = res;
-  // console.log(roleList);
+  roleList = res.map(function (obj) {
+    return obj.title;
+  });
 })};
+
+let employeeList;
+const employeeListQuery = () => {db.query('SELECT id, first_name, last_name FROM employee', (err, res) => {
+  if (err) throw err;
+  employeeList = res.map(function (obj) {
+    return obj.first_name +","+ obj.last_name;
+    });
+    // console.log(employeeList);
+
+})
+
+};
 
 
   const actionQuestion = [
@@ -55,14 +68,17 @@ const db = mysql.createConnection({
     var datetime = new Date();
     console.log(datetime);
 
-    departmentListQuerry();
-    roleListQuerry();
-    init();
+    departmentListQuery();
+    roleListQuery();
+    employeeListQuery();
 
+
+    init();
   });
 
     
   init = () => {
+        console.log("Init");
     // Initialize
     selectAction();
 
@@ -189,7 +205,8 @@ const selectAction = () => {
 // View Records
             const loadEmployeeList = () => {
               // view all Employees
-                db.query('SELECT id AS "Employee ID", first_name AS "Employee First Name", last_name AS "Employee Last Name" FROM employee', (err, res) => {
+                // db.query('SELECT id AS "Employee ID", first_name AS "Employee First Name", last_name AS "Employee Last Name" FROM employee', (err, res) => {
+                db.query('SELECT A.id AS "Employee ID", A.first_name AS "Employee First Name", A.last_name AS "Employee Last Name", roles.title AS "Title", roles.salary AS "Salary", department.department_name AS "Department", B.first_name AS "Manager First Name", B.last_name AS "Manager Last Name" FROM employee A     LEFT JOIN roles ON roles_id=roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employee B on B.id = A.manager_id;', (err, res) => {
                   if (err) throw err;
                   console.table(res);
                   init();
@@ -242,12 +259,24 @@ const selectAction = () => {
                     choices: departmentList,
                   }])                 
                 .then((answer) => {
-                const query = 'INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)';
-                db.query(query, [answer.title, answer.salary, answer.department], (err,res) =>{
-                if (err) throw err;
-                console.log(`Records updated`);
-              });
-              });
+const query_s = 'SELECT id FROM department WHERE department_name = ?';
+db.query(query_s, [answer.department], (err,res_s) =>{
+  if (err) throw err;
+
+  id_query = res_s.map(function (obj) {
+    return obj.id;
+  });
+
+  const query = 'INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)';
+  db.query(query, [answer.title, answer.salary, id_query], (err,res) =>{
+  if (err) throw err;
+  console.log(`Records updated`);
+  init();
+});
+});
+});
+
+
               };
 
 
@@ -264,6 +293,7 @@ const selectAction = () => {
                 db.query(query, [answer.departmentName], (err,res) =>{
                 if (err) throw err;
                 console.log(`Records updated`);
+                init();
               });
               });
               };
@@ -286,25 +316,45 @@ const selectAction = () => {
                     type: 'list',
                     message: 'Select new employee role:',
                     name: 'role',
-                    choices: [1, 2, 3],
+                    choices: roleList,
                   },
                   {
                     type: 'list',
                     message: 'Select new employee manager:',
                     name: 'manager',
-                    choices: [1, 2, 3],
+                    choices: employeeList,
                   }
                 ])                 
                 .then((answer) => {
-                const query = 'INSERT INTO department (department_name) VALUES (?)';
-                db.query(query, [answer.firstName, answer.lastName, answer.role, answer.manager_id], (err,res) =>{
+                  const query_i = 'SELECT id FROM roles WHERE title = ?';
+                  db.query(query_i, [answer.role], (err,res_id) =>{
+                    if (err) throw err;
+                  
+                    id_query = res_id.map(function (obj) {
+                      return obj.id;
+                    });
+
+                    const query_mn = 'SELECT id FROM employee WHERE first_name = ? AND last_name = ?';
+                    let managerName = answer.manager.split(",");
+                    db.query(query_mn, [managerName[0], managerName[1]], (err,res_mn) =>{
+                      if (err) throw err;
+                    
+                      mn_query = res_mn.map(function (obj) {
+                        return obj.id;
+                      });
+
+                const query = 'INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUES (?, ?, ?, ?)';
+                db.query(query, [answer.firstName, answer.lastName, id_query, mn_query], (err,res) =>{
                 if (err) throw err;
                 console.log(`Records updated`);
+                init();
               });
               });
-              };
+              });
+            });
+            };
 
-              // Modify
+// Modify
 
     const modifyEmployee = () => {
     inquirer
@@ -313,279 +363,63 @@ const selectAction = () => {
       type: 'list',
       message: 'Select the employee:',
       name: 'employee',
-      choices: [1, 2, 3],
+      choices: employeeList,
     }, 
     {
       type: 'list',
-      message: 'Select the new manager:',
-      name: 'manager',
-      choices: [1, 2, 3],
+      message: 'Select new employee role:',
+      name: 'role',
+      choices: roleList,
     }
+
+    // {
+    //   type: 'list',
+    //   message: 'Select the new manager:',
+    //   name: 'manager',
+    //   choices: employeeList,
+    // }
   ])                 
   .then((answer) => {
-  const query = 'UPDATE employee SET manager_id = ? WHERE id = ?';
-  db.query(query, [answer.manager_id, answer.id], (err,res) =>{
+
+    const query_en = 'SELECT id FROM employee WHERE first_name = ? AND last_name = ?';
+    let employeeName = answer.employee.split(",");
+    db.query(query_en, [employeeName[0], employeeName[1]], (err,res_en) =>{
+      if (err) throw err;
+    
+      eid_query = res_en.map(function (obj) {
+        return obj.id;
+      });
+
+      const query_i = 'SELECT id FROM roles WHERE title = ?';
+      db.query(query_i, [answer.role], (err,res_id) =>{
+        if (err) throw err;
+      
+        id_query = res_id.map(function (obj) {
+          return obj.id;
+        });
+
+
+
+      // const query_mn = 'SELECT id FROM employee WHERE first_name = ? AND last_name = ?';
+      // let managerName = answer.manager.split(",");
+      // db.query(query_mn, [managerName[0], managerName[1]], (err,res_mn) =>{
+      //   if (err) throw err;
+      
+      //   mid_query = res_mn.map(function (obj) {
+      //     return obj.id;
+      //   });
+
+
+  const query = 'UPDATE employee SET roles_id = ? WHERE id = ?';
+  db.query(query, [id_query, eid_query], (err,res) =>{
   if (err) throw err;
   console.log(`Records updated`);
+  init();
+});
+});
 });
 });
 };
               
             
               // db.end();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const roleQuestion = [
-  
-//     {
-//       type: 'list',
-//       message: 'What is the role of the teammate you want to add?',
-//       name: 'addNew',
-//       choices: ['Manager','Engineer', 'Intern', 'Finish the Team'],
-//     }];
-
-// const questions = [
-  
-//     {
-//         type: 'input',
-//         name: 'name',
-//         message: 'Enter the Employee Name?',
-//       },
-    
-//       {
-//         type: 'input',
-//         name: 'id',
-//         message: 'Enter the Employee ID:',
-//       },
-    
-//       {
-//         type: 'input',
-//         name: 'email',
-//         message: 'Enter the Employee Email Address:',
-//       }];
-
-//   const continueQuestion = [
-//       {
-//         type: 'list',
-//         name: 'continue',
-//         message: 'Do you want to add a new teammate?',
-//         choices:['Yes','No'],
-//       }];
-
-// // Specific Questions
-//     const managerQuestion = [
-//       {
-//       type: 'input',
-//       name: 'officeNumber',
-//       message: 'Enter the office number:',
-//       }];
-
-//     const engineerQuestion = [
-//       {
-//         type: 'input',
-//         name: 'github',
-//         message: 'Enter the GitHub username:',
-//       }];
-
-//       const internQuestion = [
-//         {
-//           type: 'input',
-//           name: 'school',
-//           message: 'Enter the school name:',
-//         }];
-  
-
-  
-// //Create a function to initialize app
-//   function init() {
-//     console.log("Enter the Manager information:")
-//     employeeQuestions('Manager');
-//   }
- 
-//   function employeeQuestions(roleIn){
-//       inquirer
-//       .prompt(questions)
-         
-//       .then((data) => {
-//         switch(roleIn){
-//           case 'Manager':
-//             inquirer
-//             .prompt(managerQuestion)      
-//             .then((datas) => {
-//               data.officeNumber = datas.officeNumber;
-//               const newTeamMember = new Manager(data);
-//               newTeamMember.role = newTeamMember.getRole();
-//               newTeamMember.specific=newTeamMember.getofficeNumber();
-//               console.log(`New Team Member created: ${newTeamMember.role}`);
-//               createCard(newTeamMember);
-//               proceedQuestion();
-//             });
-//             break;
-            
-//           case 'Engineer':
-//             inquirer
-//             .prompt(engineerQuestion)      
-//             .then((datas) => {
-//               data.github=datas.github;
-//               const newTeamMember = new Engineer(data);
-//               newTeamMember.role = newTeamMember.getRole();
-//               newTeamMember.specific=newTeamMember.getGithub();
-//               console.log(`New Team Member created: ${newTeamMember.role}`);
-//               createCard(newTeamMember);
-//               proceedQuestion();
-//             });
-//             break;
-        
-//           case 'Intern':
-//             inquirer
-//             .prompt(internQuestion)      
-//             .then((datas) => {
-//               data.school=datas.school;
-//               const newTeamMember = new Intern(data);
-//               newTeamMember.role = newTeamMember.getRole();
-//               newTeamMember.specific=newTeamMember.getSchool();
-//               console.log(`New Team Member created: ${newTeamMember.role}`);
-//               createCard(newTeamMember);
-//               proceedQuestion();
-//             });
-//             break;
-            
-//             default:
-//               createHTML();            
-//           }         
-
-
-//       });
-  
-//   }
-
-// function proceedQuestion(){
-//   inquirer
-//   .prompt(continueQuestion)
-    
-//   .then((data) => {
-//     console.log(data);
-//     if (data.continue === 'Yes'){
-//       role();
-//     }
-//     else{
-//       createHTML();  
-//     }
-//   });
-// }
-
-//   function role(){
-//     inquirer
-//     .prompt(roleQuestion)       
-//     .then((data) => {
-//       //   writeToFile('filename', genMarkdown.generateMarkdown(data));
-//         console.log(data);    //Debug
-//         if (data.addNew =='Finish the Team'){
-//           createHTML(); 
-//         }
-//         else{
-//           employeeQuestions(data.addNew);
-//         }
-//     });
-// }
-
-
-
-
-// const headerHTML = `<!doctype html>
-// <html lang="en">
-//   <head>
-//     <meta charset="utf-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1">
-//     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-//     <title>Team Profile Page</title>
-//   </head>
-//   <body>
-
-//   <!-- Sart Jumbo -->
-//     <div class="jumbotron jumbotron-fluid bg-primary">
-//         <div class="container text-white text-center">
-//           <h1 class="display-4">Team Profile Page</h1>
-//           <p class="lead">Software Development Team</p>
-//         </div>
-//       </div>
-// <!-- End Jumbo -->
-// <!-- Start Card Container -->
-// <div class="container">
-//     <div class="row gx-0">
-//     <!-- Cards -->`;
-
-// const footerHTML = `<!-- /Cards -->
-// </div>
-// </div>
-// <!-- End Card Container -->
-// <!-- Scripts -->
-// <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-// <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-// </body>
-// </html>`;
-
-// function createCard(newTeamMember){
-
-//   const newCard = `
-//   <div class="card m-2" style="width: 18rem;">
-//   <div class="card-header text-center">
-//     <h5 class="card-title">${newTeamMember.name}</h5>
-//     <h6 class="card-subtitle mb-2 text-muted">${newTeamMember.role}</h6>
-//   </div>
-//     <div class="card-body">
-//       <p class="card-text">ID: ${newTeamMember.id}</p>
-//       <p class="card-text">Email: ${newTeamMember.email}</p>
-//       <p class="card-text">${newTeamMember.specific}</p>
-//     </div>
-//   </div>`;
-
-//   // cardArray.push(newCard);
-//   // console.log(cardArray);
-//   cardString= cardString.concat(newCard);
-//     // console.log(`Card String ${cardString}`);
-
-// }
-
-// function createHTML(){
-
-//   // let bodyHTML = cardArray.toString();
-//     // let fullHTML = headerHTML+bodyHTML+footerHTML;
-
-//     let fullHTML = headerHTML+cardString+footerHTML;
-
-//   // console.log(fullHTML);
-
-//   fs.writeFile('./dist/index.html', fullHTML, (err) =>
-//   err ? console.log(err) : console.log('Successfully created index.html!')
-// );
-// }
-
-//   // Function call to initialize app
-//   init();
